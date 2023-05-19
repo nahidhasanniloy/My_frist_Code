b5e3ba30ce38052d38fb1bdbd5e0fb61f5e06c41
nahidhasanniloy <nahidhasanniloy09876@gmail.com>
2023-05-19 09:33:56 -0700

    first commit

---
+++ asignment.js
@@ -0,0 +1,78 @@
+// ১. Variable কি ?
+
+
+
+//  EXample;
+ var adnan = "nahid hasan niloy";
+ console.log(adnan);
+
+
+//  EXample;
+
+ var adnan = 746745;
+ console.log(adnan);
+
+
+
+// ২. Variable কিভাবে লিখতে হয় ?
+var adnan = 746745;
+console.log(adnan);
+
+
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+// EXample;
+var adnan = " nahid hasan niloy";
+console.log(adnan);
+
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// EXample;
+var adnan = 746745;
+console.log(adnan);
+
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+// EXample;
+var adnan = true;
+console.log(adnan);
+
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+var rochona = " hey i'am adnan nahid and you ,can i ask ane quistion ";
+console.log(rochona.toUpperCase());
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+ example :
+ arithmetic operator;
+ logical operator;
+ asignment operator ;
+
+ 
+
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+var hi = -24;
+var sum = Math.abs(hi);
+console.log(sum)
+
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+var hi1 = -23;
+var sum = Math.ceil(hi1);
+console.log(sum)
+
+
+
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন । 
+
+
+
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+ 
+// asignment.js - GitHub Link 
---
+++ hello.js
@@ -0,0 +1,3 @@
+var nahid = -219836743;
+var sum =Math.celi(nahid);
+console.log(sum)
